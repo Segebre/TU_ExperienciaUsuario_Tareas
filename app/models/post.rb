19e63a77b21c8323efdf37953028c2042fb0b8b9
vagrant <vagrant@precise32.(none)>
2016-11-13 11:21:50 +0000

    Antes de retocarlo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
+  has_many :comments
   validates :message, presence: true, length: { maximum: 300 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 SuperBook::Application.routes.draw do
-  get "comments/new"
-  get "comments/edit"
-  get "comments/New"
-  get "comments/Edit"
-  resources :posts
+  resources :posts do
+    resources :comments, except: [:index, :show]
+  end
   resources :memberships, except: :show
 
   # The priority is based upon order of creation: first created -> highest priority.
