c43e7633a6fd68108c72cce93af6cb5a44133b44
Juan Enrique Segebre Zaghmout <juansegebre@gmail.com>
2016-12-02 17:44:28 -0600

    ya estan la 3

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,7 +5,16 @@ class Character < ActiveRecord::Base
 
   validates :name, :email, presence: true, uniqueness: true
 
+  after_save :default_avatar_url
+
   def self.roles
     ["Heroe", "Villain"]
   end
+
+  protected
+    def default_avatar_url
+      if self.avatar_url.blank?
+        update_column(:avatar_url, 'https://tracker.moodle.org/secure/attachment/30912/f3.png')
+      end
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ActiveRecord::Base
   belongs_to :character
   has_many :comments, :dependent => :delete_all
   validates :message, presence: true, length: { maximum: 300 }
+
+  before_create :default_scheduled_at
+
+  protected
+    def default_scheduled_at
+      if self.scheduled_at == nil
+        self.scheduled_at = Time.now
+      end
+    end
 end
