cc6a837c05de583094e40e56329ef0c7d29d1dec
Shammy <jose.shamm@gmail.com>
2016-12-11 04:14:09 -0600

    Back to my version

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ SuperBook::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get '/characters/:id/posts', to: 'characters#posts', as: :character_posts
-  get 'memberships/:id/characters', to: 'memberships#characters', as: :membership_characters
+  get '/characters/:id/posts' => 'characters#posts'
+  get 'memberships/:id/characters' => 'memberships#characters'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
